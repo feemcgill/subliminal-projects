a693a53d8613ca0bb2e73f762e33d3d574334e25
Brendan Sheehan <feemcgill@gmail.com>
2020-10-16 12:54:10 -0700

    projects

--- pages/projects/_slug.vue
+++ pages/projects/_slug.vue
@@ -1,7 +1,24 @@
 
 <template>
-  <div v-if="project" id="page-exhibition">
-    <h1>{{project.title}}</h1>
+  <div v-if="project" id="page-project" class="container">
+    <div v-for="(row, index) in project.ProjectBuilder.project" v-bind:key="index" v-bind:class="'project-row cols-' + row.columns.length">
+
+      <div v-for="(column, colindex) in row.columns" v-bind:key="colindex" v-bind:class="'project-col '+ column.type+ ' '+column.verticalAlign">
+      
+        <div v-if="column.type == 'image'" v-bind:class="'image-wrap ' + column.imageFit">
+          <FadeImage :src="column.image.sourceUrl" :srcset="column.image.srcSet" :alt="column.image.altText" />
+          <div v-if="column.imageCaption"  class="caption" v-html="column.imageCaption"></div>
+        </div>
+      
+        <div v-if="column.type == 'content'">
+          <div class="formatted-content" v-html="column.content"></div>
+        </div>
+
+        <div v-if="column.type == 'empty'" />
+      
+      </div>
+
+    </div>
   </div>
 </template>
 <script>
@@ -23,7 +40,7 @@ export default {
           console.log(error)
         },
         result({data}) {
-          if(!data.exhibition){
+          if(!data.project){
             this.$nuxt.error({ statusCode: 404 })
           }  
         },        
@@ -39,7 +56,6 @@ export default {
               slug
               title
               ProjectBuilder {
-                fieldGroupName
                 project {
                   ... on Project_Projectbuilder_Project_Row {
                     fieldGroupName
@@ -48,6 +64,7 @@ export default {
                       imageFit
                       imageCaption
                       content
+                      verticalAlign
                       image {
                         altText
                         sourceUrl(size: LARGE)
@@ -64,5 +81,81 @@ export default {
   }   
 }
 </script>  
-  
-  
+
+
+<style lang="scss" scoped>
+  #page-project {
+    margin-bottom: 2vw;
+  }
+  .project-row {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    &.cols-1 {
+      .project-col {
+        width: 100%;
+      }
+    }
+    &.cols-2 {
+      .project-col {
+        width: 50%;
+        @include breakpoint(small) {
+          width: 100%;
+        }        
+      }
+    }
+    &.cols-3 {
+      .project-col {
+        width: 33.3333334%;
+        @include breakpoint(small) {
+          width: 100%;
+        }        
+      }
+    }
+    &.cols-4 {
+      .project-col {
+        width: 25%;
+        @include breakpoint(small) {
+          width: 100%;
+        }        
+      }
+    }
+    .project-col {
+      position: relative;
+
+      &.top {
+        align-self: flex-start;
+      }
+      &.bottom {
+        align-self: flex-end;
+      }
+      &.content {
+        padding: 2vw;
+      }
+      &.image {
+        img {
+          width: 100%;
+          height: auto;
+          display: block;
+        }
+        .bleed {
+          img {
+            //height: 100%;
+          }
+        }
+        .contain {
+          padding: 2vw;
+        } 
+        .caption {
+          position: absolute;
+          bottom: 1em;
+          left: 1em;
+          z-index: 100;
+          background-color: $light;
+          padding: 0.25em 0.5em;
+          max-width: 300px;
+        }     
+      }
+    }           
+  }
+</style>
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -8,39 +8,138 @@
         <h2>Featured Project</h2>
       </div>
       <div class="info">
-        In addition to creating brick and mortar and online art exhibitions, Subliminal Projects curates, consults and produces a multide of projects and collaborations with like-minded brands, retail spaces, event venues and comercial spaces. To inquire on a collaborative project please email info@subliminalprojects.com.
+        In addition to creating brick and mortar and online art projects, Subliminal Projects curates, consults and produces a multide of projects and collaborations with like-minded brands, retail spaces, event venues and comercial spaces. To inquire on a collaborative project please email info@subliminalprojects.com.
       </div>
     </section>
-    <section class="projects container grid">
-      <div class="project">
-        <div class="img">
-          <img src="https://subliminalprojects.d-e-v.group/wp-content/uploads/2014/10/The-drive-in-floods...SML_.jpg" alt="">
+    <section v-if="projects" class="projects container grid">
+
+      <nuxt-link :to="'/projects/'+project.node.slug" class="project" v-for="project in projects.edges" v-bind:key="project.node.slug" >
+        <div class="img" v-if="project.node.featuredImage">
+          <FadeImage :src="project.node.featuredImage.node.sourceUrl" :srcset="project.node.featuredImage.node.srcSet" :alt="project.node.featuredImage.node.altText" />
         </div>
         <div class="details">
-          <h3>Project Title</h3>
+          <h3>{{project.node.title}}</h3>
         </div>
-      </div>
-      <div class="project">
-        <div class="img">
-          <img src="https://subliminalprojects.d-e-v.group/wp-content/uploads/2016/05/NewTranslations-2-1600x1498.jpg" alt="">
-        </div>
-        <div class="details">
-          <h3>Project Title</h3>
-        </div>
-      </div>
-      <div class="project">
-        <div class="img">
-          <img src="https://subliminalprojects.d-e-v.group/wp-content/uploads/2012/03/Jamie-Reid-Ad-Front.jpg" alt="">
-        </div>
-        <div class="details">
-          <h3>Project Title</h3>
-        </div>
-      </div>
+      </nuxt-link>
     </section>
+      <div v-if="projects" class="pagination">
+        <div v-if="projects.pageInfo.hasNextPage" class="load-more">
+          <div v-if="$apollo.loading" class="loading">Loading...</div>
+          <div v-else @click="loadMore()" class="primary-button">Load More</div>
+        </div>    
+        <div v-else class="all-loaded">
+          <div v-if="projects.edges.length == 0" class="none-found">Sorry, we couldn't find any projects matching this criteria.</div>
+          <!-- <div v-else>All {{projects.edges.length}} projects loaded</div> -->
+        </div>
+      </div>
+
   </div>
 </template>
 
+
+
+
+<script>
+import gql from 'graphql-tag'
+import FadeImage from '~/components/FadeImage'
+
+const projects_per_load = 3
+
+export default {
+  components: {
+    FadeImage
+  },
+  data() {
+    return {
+      foundPosts: null,
+      queryCursor: null,
+      displayedPosts: []
+    };
+  },  
+  mounted() {
+    //this.$store.commit('setNavColor', 'black')
+  },
+  methods: {
+    loadMore(){
+      this.$apollo.queries.projects.fetchMore({
+        variables: {
+          after: this.projects.pageInfo.endCursor,
+          first: projects_per_load         
+        },
+        updateQuery: (previousResult, { fetchMoreResult }) => {
+          const newEdges = fetchMoreResult.projects.edges;
+          const pageInfo = fetchMoreResult.projects.pageInfo;          
+          return {
+            projects: {
+              __typename: previousResult.projects.__typename,
+              edges: [...previousResult.projects.edges, ...newEdges],
+              pageInfo
+            }
+          }
+        }
+      })
+    }    
+  },
+  apollo: {
+      projects: {
+        result(data) {
+          console.log('past data', data)
+        },        
+        variables: {
+          after: null,
+          first: projects_per_load
+        },
+        query: gql`
+          query Projects($first: Int, $after: String) {
+            projects(first: $first, after: $after) {
+              edges {
+                node {
+                  slug
+                  title
+                  featuredImage {
+                    node {
+                      sourceUrl(size: MEDIUM)
+                      altText
+                      srcSet(size: MEDIUM)
+                      mediaDetails {
+                        width
+                        height
+                      }
+                    }
+                  }
+                }
+              }
+              pageInfo {
+                hasNextPage
+                endCursor
+              }
+            }
+          }
+        `
+    }
+  }   
+}  
+// https://hackernoon.com/how-to-build-a-load-more-button-with-vue-js-and-graphql-5e6de1b61a6b
+</script>
+
+
+
 <style lang="scss" scoped>
+
+
+  .exhibition-thumb,
+  .past-exhibition {
+    @include thirds;
+  }
+  .pagination {
+    margin: $factor;
+    text-align: center;
+  }
+  .load-more {
+    min-height: $factor * 2;
+  }
+
+
   .featured-project {
     display: flex;
     flex-wrap: wrap;
