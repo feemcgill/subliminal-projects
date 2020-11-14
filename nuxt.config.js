09727ad3f57319b93864125806613a1c804f8e4b
Brendan Sheehan <feemcgill@gmail.com>
2020-11-13 21:27:53 -0800

    add default meta description

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Subliminal Projects - Art Gallery Los Angeles - Shepard Fairey' }
+      { hid: 'description', name: 'description', content: 'Subliminal Projects - Art Gallery Los Angeles - Shepard Fairey' },
+      { hid: 'og:description', name: 'og:description', content: 'Subliminal Projects - Art Gallery Los Angeles - Shepard Fairey' }
     ],
     link: [
       // { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
